6df5ceebd9c41583b26f6fb677bc88f5fecb7b63
pablomartan <pablomartan@kolabnow.com>
2023-04-10 17:25:38 +0200

    feat: remove placeholder and add LengthControl components for break and session

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { LengthControl } from './features/lengthControl/LengthControl'
 
 export const App = () => {
   return(
     <>
       <div id="app">
-        <h1>Hello World!</h1>
+        <LengthControl name="break" value="5" />
+        <LengthControl name="session" value="25" />
       </div>
     </>
   )
