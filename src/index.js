55c80d8f825a822dc109bd1f0884d954ad91a1ff
pablomartan <pablomartan@kolabnow.com>
2023-04-10 17:32:42 +0200

    feat: connect react and redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { App } from './js/App';
+import { store } from './js/app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
