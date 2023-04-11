c2f3881b110f84fa81419ca277a6c7263d5ab134
pablomartan <pablomartan@kolabnow.com>
2023-04-11 17:30:11 +0200

    feat: add functions to increase and decrease

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { LengthControl } from './features/lengthControl/LengthControl'
+import { useSelector } from 'react-redux';
+import { LengthControl } from './features/lengthControl/LengthControl';
+import {
+  breakInc,
+  breakDec,
+  sessionInc,
+  sessionDec,
+  reset,
+  stateLength
+} from './features/lengthControl/lengthControlSlice';
 
 export const App = () => {
+  const length = useSelector(stateLength);
+
   return(
     <>
       <div id="app">
-        <LengthControl name="break" value="5" />
-        <LengthControl name="session" value="25" />
+        <LengthControl name="break" value={length.brk} inc={breakInc} dec={breakDec} />
+        <LengthControl name="session" value={length.session} inc={sessionInc} dec={sessionDec} />
       </div>
     </>
   )
