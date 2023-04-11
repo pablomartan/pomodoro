3a11adc73698335311ce9c57d3fd3b5763a04514
pablomartan <pablomartan@kolabnow.com>
2023-04-11 23:56:28 +0200

    feat: add Timer component to App

--- src/js/App.js
+++ src/js/App.js
@@ -9,15 +9,21 @@ import {
   reset,
   stateLength
 } from './features/lengthControl/lengthControlSlice';
+import { Timer } from './features/timer/Timer';
+import {
+  stateTimer 
+} from './features/timer/timerSlice';
 
 export const App = () => {
   const length = useSelector(stateLength);
+  const timer = useSelector(stateTimer);
 
   return(
     <>
       <div id="app">
         <LengthControl name="break" value={length.brk} inc={breakInc} dec={breakDec} />
         <LengthControl name="session" value={length.session} inc={sessionInc} dec={sessionDec} />
+        <Timer />
       </div>
     </>
   )
