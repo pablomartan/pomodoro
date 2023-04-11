60d91f71c8526b8bd1193ead8d060efbe93fe2d4
pablomartan <pablomartan@kolabnow.com>
2023-04-11 23:55:40 +0200

    feat: add timerSlice reducer to store

--- src/js/app/store.js
+++ src/js/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import lengthReducer from '../features/lengthControl/lengthControlSlice';
+import timerReducer from '../features/timer/timerSlice';
 
 export const store = configureStore({
   reducer: {
     length: lengthReducer,
+    timer: timerReducer
   }
 });
