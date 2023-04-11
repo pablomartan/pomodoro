ffa9fd14fb3054f9d3f5b8b89a2f089f2d01c4b2
pablomartan <pablomartan@kolabnow.com>
2023-04-11 23:55:00 +0200

    feat: connect Timer component and timerSlice

--- src/js/features/timer/Timer.js
+++ src/js/features/timer/Timer.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import {
+  useSelector,
+  useDispatch
+} from 'react-redux';
+import {
+  stateTimer,
+  countdown,
+} from './timerSlice';
+
+let timer; 
+
+const startStopDispatcher = (state, dispatch) => {
+  if (!timer) {
+    timer = setInterval(() => dispatch(countdown()), 1000);
+  } else {
+    clearInterval(timer);
+    timer = null;
+  }
+
+};
 
 export const Timer = props => {
   const dispatch = useDispatch();
+  const state = useSelector(stateTimer);
 
   return(
     <>
       <div id="timer-label">Session</div>
-      <div id="time-left">{props.remaining}</div>
+      <div id="time-left">{state.remaining}</div>
       <div className="controls" id="timer-controls">
-        <div id="start_stop" onClick={() => dispatch(props.startStop())}>Start-Stop</div>
+        <div id="start_stop" onClick={() => startStopDispatcher(state, dispatch)}>Start-Stop</div>
         <div id="reset" onClick={() => dispatch(props.reset())}>Reset</div>
       </div>
     </>
