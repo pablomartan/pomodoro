d4ff49b278545bf450a8c4a2dcb822fad3b444a4
pablomartan <pablomartan@kolabnow.com>
2023-04-11 17:32:06 +0200

    feat: completed functionality to increase and decrease length in LengthControl component

--- src/js/features/lengthControl/LengthControl.js
+++ src/js/features/lengthControl/LengthControl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 export const LengthControl = props => {
   // TODO: controls for increment and decrement
@@ -6,14 +7,15 @@ export const LengthControl = props => {
   const incrementId = props.name + '-increment';
   const decrementId = props.name + '-decrement';
   const lengthId = props.name + '-length';
+  const dispatch = useDispatch();
   
   return(
     <div className="length-control" id={props.id}>
       <h3 className="control-label text-capitalize" id={labelId}>{props.name} length</h3>
       <div className="controls">
-        <div id={decrementId} />
-        <div id={incrementId} />
+        <button id={decrementId} onClick={() => dispatch(props.dec())}>↓</button>
         <h4 className="control-value" id={lengthId}>{props.value}</h4>
+        <button id={incrementId} onClick={() => dispatch(props.inc())}>↑</button>
       </div>
     </div>
   )
