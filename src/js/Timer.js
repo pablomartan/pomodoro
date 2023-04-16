4394baf6876fe156b86ebd5484cbf0c44104d0d9
pablomartan <pablomartan@kolabnow.com>
2023-04-16 21:48:39 +0200

    style: add new lines for readibility in Timer component

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -11,7 +11,9 @@ export const Timer = props => {
       <h3 className="text-center" id="timer-label">{state.timerLabel}</h3>
       <h4 className="text-center" id="time-left">{props.timeLeft}</h4>
       <div className="row" id="timer-controls">
-        <div className="col text-center btn btn-warning" id="start_stop" onClick={() => props.startStopHandler(state, dispatch)}>Start-Stop</div>
+        <div className="col text-center btn btn-warning" id="start_stop" onClick={() => {
+          props.startStopHandler(state, dispatch)
+        }}>Start-Stop</div>
         <div className="col text-center btn btn-danger" id="reset" onClick={() => {
           dispatch(props.reset());
         }}>
