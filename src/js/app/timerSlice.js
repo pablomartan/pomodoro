631066af5a736a1ef6e09ca50222e499df393766
pablomartan <pablomartan@kolabnow.com>
2023-04-16 11:40:47 +0200

    feat: change time left repr. from str to number
    Changed the representation of left time in counter to seconds, as it
    simplifies the state update on countdown and "mode" change (changing
    from break to work and back). The transformation of seconds to string
    with format mm:ss will be done in the timer component.

--- src/js/app/timerSlice.js
+++ src/js/app/timerSlice.js
@@ -15,7 +15,7 @@ export const timerSlice = createSlice({
     breakLength: 5,
     sessionLenght: 25,
     timerLabel: 'Session',
-    remaining: '25:00',
+    timeLeft: 25 * 60,
     intervalId: null,
     isSwitching: false,
   },
@@ -34,20 +34,20 @@ export const timerSlice = createSlice({
       if (state.sessionLenght < 60) {
         state.sessionLenght += 1;
       }
-      state.remaining = state.sessionLenght < 10 ? `0${state.sessionLenght}:00` : `${state.sessionLenght}:00`;
+      state.timeLeft = state.sessionLenght * 60;
     },
     sessionDec: state => {
       if (state.session > 1) {
         state.session -= 1;
       }
-      state.remaining = state.sessionLenght < 10 ? `0${state.sessionLenght}:00` : `${state.sessionLenght}:00`;
+      state.timeLeft = state.sessionLenght * 60;
     },
     reset: state => {
       clearInterval(state.intervalId);
       state.breakLength = 5;
       state.sessionLenght = 25;
       state.timerLabel = 'Session';
-      state.remaining = '25:00';
+      state.timeLeft = '25:00';
       state.intervalId = null;
       state.intervalId = 
       document.getElementById('beep').pause();
@@ -55,21 +55,20 @@ export const timerSlice = createSlice({
     },
     countdown: state => {
       state.isSwitching = false;
-      const timeLeft = state.remaining.split(':');
+      const timeLeft = state.timeLeft.split(':');
       const nextTime = Number(timeLeft[0]) * 60 + Number(timeLeft[1]) - 1;
-      const nextState =  addZero(Math.floor(nextTime / 60)) + ':' + addZero(nextTime % 60);
 
       if (nextTime >= 0) {
-        state.remaining = nextState;
+        state.timeLeft = nextState;
       } else {
         clearInterval(state.intervalId);
         document.getElementById('beep').play();
         state.timerLabel = state.timerLabel == 'Break' ? 'Session' : 'Break';
         state.isSwitching = true;
         if (state.timerLabel == 'Break') {
-          state.remaining = addZero(state.breakLength) + ':00';
+          state.timeLeft = state.breakLength * 60;
         } else {
-          state.remaining = addZero(state.sessionLenght) + ':00';
+          state.timeLeft = state.sessionLenght * 60;
         }
         state.isSwitching = false;
       }
